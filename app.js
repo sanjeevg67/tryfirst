967d619c765d0689b314491387e7efaba2d81cc8
Sanjeev Goel <sanjeevg67@yahoo.com>
2017-09-28 13:08:01 +0530

    Added about controller

--- app.js
+++ app.js
@@ -28,3 +28,7 @@ app.controller('HomeController', function($scope) {
 app.controller('BlogController', function($scope) {
   $scope.message = 'Hello from BlogController';
 });
+
+app.controller('AboutController', function($scope) {
+  $scope.message = 'Hello from AboutController';
+});
